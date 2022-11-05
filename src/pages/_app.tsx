86134f4acf232bb07a3b6f231f97b76eae189e16
Hunter Bianchi <hunter.bianchi@tutanota.com>
2022-11-05 02:35:57 -0300

    (0.0.4) Move SSR from pages/index.tsx to pages/app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { parseCookies } from 'nookies'
 import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,12 +16,82 @@ export default function App({ Component, pageProps }: AppProps) {
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
-
+      <meta name="theme-color" content="#ff44aa" />
 
     </Head>
     <GlobalStyles />
     <Component {...pageProps} />
     </>
   )
+}
+
+export async function getServerSideProps(context: any) {
+
+  const cookies = parseCookies(context)
+
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+
+  const chain: Array<any> = require('../json/chain.json')
+  const genesis = chain[0]
+  console.log(genesis)
+
+  const orderList = {}
+  const offerList = {}
+  const chainHeader = {}
+
+  // console.log(`\nGENESIS HASH:\n${SHA256(genesis.timestamp + genesis.previousHash + JSON.stringify(genesis.data) + genesis.nonce).toString()}\n`)
+
+  try {
+    await fetch(`${baseURL}/chain`, {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        type: 'get-chain-header',
+        data: {}
+      })
+    }).then((res: any) => res.json()).then((res: any)=>{
+      const { data } = res
+      const newChainHeader = data
+
+      Object.assign(chainHeader, newChainHeader)
+    })
+  } catch (error) {
+    throw new Error(`Error while fetching chain from ${baseURL}/chain`)
+  } finally {
+    try {
+      await fetch(`${baseURL}/chain`, {
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          type: 'get-chain',
+          data: chain
+        })
+      }).then((response: any) => response.json()).then((response: any)=>{
+        const { data } = response
+        const newChain = data
+  
+        Object.assign(chain, newChain)
+  
+      })
+    }catch (err) {
+      throw new Error(`Error while fetching chain from ${baseURL}/chain`)
+    }
+
+    return {
+      props: {
+        baseURL,
+        chain,
+        privateKey: cookies.PRIVATE_KEY ? cookies.PRIVATE_KEY : '',
+        offerList,
+        orderList,
+        chainHeader,
+        countDown: 10,
+      },
+    }
+  }
+
 }
