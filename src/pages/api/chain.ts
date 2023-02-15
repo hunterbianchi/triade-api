fa24872e29c41011f5064e48b4256ea3edc523d0
Hunter <hunter@localhost.localdomain>
2023-02-15 12:16:59 -0300

    Fix

--- src/pages/api/chain.ts
+++ src/pages/api/chain.ts
@@ -76,15 +76,15 @@ export default async function handle (req: any, res: any){
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, *'
     )
-
+    
+    const { method } = req
+    
     if (req.method === 'OPTIONS') {
         res.status(200).end()
         return
     }
-
-    const { method } = req
     
     res.setHeader('Access-Control-Allow-Origin','*')
 
