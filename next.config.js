cad65e4ac40fe044a9507ef39afb0bdc4bca75ea
Commercial <commercial@localhost.localdomain>
2023-03-19 20:56:20 -0300

    Test

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/_next/:path*",
+        source: "/_next/:path*, *",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
