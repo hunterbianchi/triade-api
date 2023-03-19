a2359c25070ea2b3c386420b09ae139842c42cca
Commercial <commercial@localhost.localdomain>
2023-03-19 20:22:35 -0300

    Test

--- src/pages/api/chain.ts
+++ src/pages/api/chain.ts
@@ -72,7 +72,7 @@ function notifyObservers(command:any){
 export default async function handle (req: any, res: any){
 
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', '*, https://triade-group.vercel.app/*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
