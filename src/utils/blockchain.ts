f2dd5d4727def2beac719c0e140549c3c4743816
Comercial <comercial@localhost.localdomain>
2024-08-23 18:59:48 -0300

    15 block limitation

--- src/utils/blockchain.ts
+++ src/utils/blockchain.ts
@@ -109,6 +109,9 @@ export class BlockChain {
     
     updateChain(newChain: Array<any>){
 
+        if(newChain.length > 15){
+            return
+        }
         if(newChain.length > this.chain.length){
             // console.log(`\t\t|\tReceived a larger (size: ${newChain.length}) chain...\n`)
             // console.log(`${newChain[0].hash}\n${this.chain[0].hash}`)
