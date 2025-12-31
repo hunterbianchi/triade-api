3445f4ed2de0f146ab2eb8fa2c846e38dbc6bf08
Fernando Molica <fernando.molica.jr@gmail.com>
2025-12-31 05:14:17 -0300

    menu

--- src/pages/api/chain.ts
+++ src/pages/api/chain.ts
@@ -130,7 +130,7 @@ const orderList: any[] = [
         timestamp: 1767054143324,
         fromAddress: '042ef6646dacb5c148271654305981d5d96324624328a17a819f81ae30b44bf9ce898e2bf955b3fdc6c5404ac0bd96e98e5569d871fdee5c44d2fe7abb3e565a37',
         quotes: 1,
-        toAddress: '',
+        toAddress: '042ef6646dacb5c148271654305981d5d96324624328a17a819f81ae30b44bf9ce898e2bf955b3fdc6c5404ac0bd96e98e5569d871fdee5c44d2fe7abb3e565a37',
         amount: 2,
         brlPrice: 17000
     }
